a0f3a39451cbafedec6c167dae8324d14dc20d2d
fos1e2 <jean-marc.brun@cegedim-activ.com>
2023-11-10 17:48:13 +0100

    customisation

--- src/main/java/com/openclassrooms/testing/calcul/service/SolutionFormatterImpl.java
+++ src/main/java/com/openclassrooms/testing/calcul/service/SolutionFormatterImpl.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.testing.calcul.service;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
 import javax.inject.Named;
@@ -9,6 +11,10 @@ public class SolutionFormatterImpl implements SolutionFormatter {
 
 	@Override
 	public String format(int solution) {
-		return String.format(Locale.FRENCH, "%,d", solution);
+		DecimalFormatSymbols fmt = DecimalFormatSymbols.getInstance(Locale.FRENCH);
+		fmt.setGroupingSeparator(' '); 		
+		DecimalFormat decimalFormat = new DecimalFormat();
+		decimalFormat.setDecimalFormatSymbols(fmt);		
+		return decimalFormat.format(solution);
 	}
 }
